13e2525164360b52c4ec5868146f530f48746cdf
shao <2314781899@qq.com>
2019-05-24 22:26:43 +0800

    add new module

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     #游戏初始化
@@ -19,16 +20,9 @@ def run_game():
     #游戏主循环
     while True:
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
-        #重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
 
