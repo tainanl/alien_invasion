31df71082cbf802a4d6671c64ebff48ee7f91100
shao <2314781899@qq.com>
2019-05-26 16:35:00 +0800

    refactor functions

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,7 +15,7 @@ def run_game():
     pygame.display.set_caption('Alien Invasion')
     
     #创建飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     #游戏主循环
     while True:
--- ship.py
+++ ship.py
@@ -1,10 +1,11 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         '''初始化飞船并设置初始位置'''
         #设置整个窗口为活动范围（自理解）
         self.screen = screen
+        self.settings = ai_settings
         
         #加载图片，无image类，返回Surfade对象（官方文档指出）
         self.image = pygame.image.load('images/ship.bmp')
@@ -17,16 +18,22 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        #存储小数
+        self.center = float(self.rect.centerx)
+
         #移动标志
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         '''根据移动标志调整飞船位置'''
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        #更新center，加上速度指，许多属性只接受整数（官方文档）
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.settings.ship_speed_factor
+
+        self.rect.centerx = self.center
 
     def blitme(self):
         '''在指定位置绘制飞船'''
